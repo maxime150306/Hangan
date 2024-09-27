a01d72647d73ac6f28def7ae211fd60b3d8fee49
mthomas <thomas.melin@ynov.com>
2024-09-27 11:21:49 +0200

    hangman

--- fonction/hangman.go
+++ fonction/hangman.go
@@ -2,6 +2,10 @@ package hangman
 
 import "fmt"
 
+// Hangman est une fonction qui prend une string en parametre et qui permet au joueur 
+// de trouver le mot en devinant des lettres. Si le joueur se trompe, il perd une vie.
+// Lorsque le joueur a trouve le mot, il a gagne. Lorsqu'il a perdu toutes ses vies, 
+// il a perdu.
 func Hangman(s string) {
 	var try int = 10
 	var motcache string
@@ -17,15 +21,29 @@ func Hangman(s string) {
 
 		}
 		var nvmot string
+		var bonneLettre bool = false
 		for i, char := range s {
 			if string(char) == lettre {
 				nvmot = nvmot + lettre
+				bonneLettre = true
 			} else {
 				nvmot = nvmot + string(motcache[i])
 			}
+
 		}
 		motcache = nvmot
-		fmt.Println(motcache)
+		if !bonneLettre {
+			try = try - 1
+			fmt.Println("Mauvaise lettre, il vous reste", try, "vies")
+		}	
+		if motcache == s {
+			fmt.Println("Bravo, vous avez gagné !")
+			break
+		}
+		if try == 0 {
+			fmt.Println("Dommage, vous avez perdu !")
+			break
+		}
 		
 	}
 }
